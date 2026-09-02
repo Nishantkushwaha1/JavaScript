bc16e4cc72e5e0dac5e55bf34deb729ff68b544f
Nishant <nishantkushwahadeoria@gmail.com>
2026-09-02 22:39:00 +0530

    added data type

---
+++ Basics/data_type.js
@@ -0,0 +1,26 @@
+"use strict";      //---> treat all JS code as newer version.
+
+let username = "Nishant"
+let age = 20
+let isLoggedIn = false
+let state;
+let temp = null;
+
+
+// Data Types: 
+
+// number
+// bigint
+// string ---> ""
+// Boolean  ---> true/false
+// null   ---> standalone value
+// undefined
+// symbol ---> unique
+
+console.log(typeof username);           // string
+console.log(typeof age);                // number
+console.log(typeof isLoggedIn);         // boolean
+console.log(typeof state);              // undefined
+console.log(typeof temp);               // object
+
+
