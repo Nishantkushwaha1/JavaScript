5a5baffc1f2303ba0caa27e11a14f32276513b4a
Nishant <nishantkushwahadeoria@gmail.com>
2026-09-08 20:37:19 +0530

    Added Background Color Changer files

---
+++ PROJECTS/Color Changer/script.js
@@ -0,0 +1,16 @@
+
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach(function (button) {
+
+    button.addEventListener('click', function(e) {
+
+        // console.log(e)
+        // console.log(e.target)
+
+        body.style.background = e.target.id;
+
+    })
+})
+
