8c3d665973fcda777964f0edb818db8489f7cf00
Nishant <nishantkushwahadeoria@gmail.com>
2026-09-26 11:45:11 +0530

    Added loops.js

---
+++ Loops/loop.js
@@ -0,0 +1,32 @@
+//For loop
+
+arr = [5, 8, 3, 2, 1]
+
+for(let i=0; i<arr.length; i++)
+{
+    // console.log(arr[i]);
+}
+
+
+// ForOf loop
+
+arr2 = [3, 6, 8, 9, 2]
+
+for(let i of arr2)
+{
+    // console.log(i)
+}
+
+
+// ForEach loop
+
+arr3 = [2, 4, 6, 8, 0]
+
+arr3.forEach((e, i) =>{
+    console.log(e, i)
+})
+
+//Here 1st one is for value, 2nd one is for index and 3rd is for array.
+// array.forEach((value, index, array) => {
+    // console.log(value, index, array);
+// });
