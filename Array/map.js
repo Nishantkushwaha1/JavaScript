e374a9416bc46203713712ed131d289bbaf2230a
Nishant <nishantkushwahadeoria@gmail.com>
2026-09-26 15:22:31 +0530

    Added map.js

---
+++ Array/map.js
@@ -0,0 +1,25 @@
+// Basics
+function add10(ele){
+    return ele + 10;
+}
+let arr = [1, 6, 3, 8]
+console.log(arr)
+
+
+//Method 1:
+arr = arr.map(function(ele){
+     return ele*ele
+})
+console.log(arr)
+
+
+// Method 2:
+arr = arr.map((ele)=>{
+    return ele/10;
+})
+console.log(arr)
+
+
+//Method 3:
+arr = arr.map(ele => ele*2)
+console.log(arr)
